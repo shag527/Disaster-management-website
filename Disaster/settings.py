197796b504c7b430c975debec0beb5992e57a2df
shag527 <shagunkatoch527@gmail.com>
2020-07-31 09:56:19 +0530

    Security issues fixed

--- Disaster/settings.py
+++ Disaster/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@q76!e5nkbozm#st#0w=*#-zyzfmtk@u4d02f$%wil9p6f23lz'
+#SECRET_KEY = '@q76!e5nkbozm#st#0w=*#-zyzfmtk@u4d02f$%wil9p6f23lz'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
